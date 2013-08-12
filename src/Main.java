9f959348a698b58cf3ff099364f51aaf53668896
KOlegA <kolega.ua@gmail.com>
2013-08-13 00:40:26 +0300

    запускаются и работают все 3 вида игры, осталось допиливание

--- src/Main.java
+++ src/Main.java
@@ -100,8 +100,6 @@ public class Main {
 
 	        }
 
-	        System.out.println(column + " " + row);
-
             move(column, row, moveOrder);
 
             gameBoard.setChipOnBoard(startColumn, startRow,chipCurrentMove);
