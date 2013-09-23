cc125871089b6401a03367ef86ed6dfa4d922306
Олег <kolega.ua@gmail.com>
2013-09-23 18:49:29 +0300

    добавил немного комментариев

--- src/virtuals/gardner/Gardner.java
+++ src/virtuals/gardner/Gardner.java
@@ -1,6 +1,5 @@
 package virtuals.gardner;
 
-import java.util.Arrays;
 import java.util.HashSet;
 
 /**
@@ -58,12 +57,26 @@ public class Gardner {
 	public boolean isWin(int x, int y, char chip) {
 		HashSet<String> win = new HashSet<>();
 		for (int i = 0; i < 4; i++) {
-			win.addAll(checkWin(x, y, chip, i));
+			win.addAll(checkRow(x, y, chip, i));
+
+			/*
+			Если в куче элементов координат больше, чем один,
+			то партия считается выигранной/проигранной, т.к.
+			есть миним одна из двух клеток, которую не успевает
+			обезвредить противник.
+			 */
 			if ((win.size()) > 1) {
 				return true;
 			}
 		}
-        String[] coordinate = win.toArray(new String[0]);
+
+
+		/*
+		Это мне не нравится, надо переделывать.
+		Преобразует координаты в удобоваримый вид из стринги,
+		которая пришла из кучи.
+		 */
+		String[] coordinate = win.toArray(new String[0]);
         String[] xy = coordinate[0].split(" ");
         int[] xAndY = new int[2];
         for (int i = 0; i < xAndY.length; i++){
@@ -72,44 +85,79 @@ public class Gardner {
         }
 		return false;
 	}
-    public HashSet<String> checkWin(int xx, int yy, char chip, int direction) {
+
+	/**
+	 * В данном методе выполняется проверка победы при заполненнии указанной
+	 * фишкой победного ряда состоящего из numInTheRow количества фишек подряд.
+	 * При проверке осуществляется перебор полей на длину победного ряда
+	 * слева-направо, сверху-вниз, по диагонали слева-вниз и
+	 * по диагонали слева-вверх. При этом начальная клетка для проверки
+	 * определяется максимальным выносом на длину победного ряда влево или вверх.
+	 * После проверки граничной клетки, выполняется проверка на длину ряда для
+	 * сосденей с ней клетки в направлении проверки.
+	 *
+	 * @param xx координата клетки по Х
+	 * @param yy координата клетки по Y
+	 * @param chip фишка (камень) для которого проверяется состояние выигрыша
+	 * @param direction напраление проверки ряда
+	 * @return HeshSet координат пустых потенциально победных клеток
+	 * @exception IndexOutOfBoundsException выкидывается при попадании
+	 * проверочных координат за границы массива.
+	 */
+    public HashSet<String> checkRow(int xx, int yy, char chip, int direction) {
         int x = 0, y = 0;
-	    HashSet<String> emptySell = new HashSet<>();
-	    HashSet<String> emptySum = new HashSet<>();
+	    HashSet<String> emptySell = new HashSet<>();   //сборщик пустых полей от прохода ряда
+	    HashSet<String> emptySum = new HashSet<>();    //сборщик пустых полей от всех проходов
 
 	    start:
         for (int i = 0; i < numInTheRow; i++) {
 	        for (int k = 0; k < numInTheRow; k++) {
 		        x = xx - (numInTheRow - 1) + i + k;
-		        switch (direction) {
-			        case 0:
+		        switch (direction) {      // переключатель направлений
+			        case 0:               // проверка по горизонтали слева
 				        y = yy;
 				        break;
-			        case 1:
+			        case 1:               // проверка по вертикали сверху
 				        x = xx;
 				        y = yy - (numInTheRow - 1) + i + k;
 				        break;
-			        case 2:
+			        case 2:               // проверка по диагонали слева-сверху
 				        y = x + (yy - xx);
 				        break;
-			        case 3:
+			        case 3:               // проверка по диагонали слева-снизу
 				        y = (yy + xx) - x;
 				        break;
 		        }
 		        try {
+
+			        /*
+			         Если при проходе попадается знак отличный от
+			        проверяемого, то куча пустых клеток обнуляется и
+			        прекращается дальнейший перебор ряда.
+			         */
 			        if (gameBoard[x][y] != chip & gameBoard[x][y] != '\u0000') {
 				        emptySell.clear();
 				        continue start;
 			        }
+			        /*
+			        Здесь мы закидываем пустую клетку в кучу
+			         */
 			        if (gameBoard[x][y] == '\u0000') {
 				        emptySell.add(x + " " + y);
 			        }
+		        /*
+		        Если проверка попала за границы массива, то обнуляем
+		        кучу пустых клеток.
+		         */
 		        } catch (IndexOutOfBoundsException ex) {
 			        emptySell.clear();
 			        continue start;
 		        }
 	        }
-	        /* при отсутствии пустых полей - победа*/
+	        /*
+	        при отсутствии пустых полей - победа
+	        записываем в кучу больше одного элемента
+	        */
 	        if (emptySell.size() == 0) {
 		        System.out.println(emptySell.size());
 		        for (int g = 0; g < 3; g++) {
@@ -117,8 +165,10 @@ public class Gardner {
 		        }
 		        return emptySum;
 	        }
-	        /*если пустых полей больше одного,
-		     * то очистить счетчик пустых полей*/
+	        /*
+	        Если пустых полей больше одного,
+		    то очистить счетчик пустых полей
+		    */
 	        if (emptySell.size() > 1) {
 		        emptySell.clear();
 	        }
